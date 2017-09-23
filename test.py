902d64935a1a357a9458ef5c6754f9da9dac2031
tom dorrington <dorrez2k8@hotmail.co.uk>
2017-09-23 09:27:51 +0100

    Add poppet

--- test.py
+++ test.py
@@ -7,10 +7,14 @@ def tasty_is_good():
 def specs_is_good():
     print("specks is cool")
 
+def poppet():
+    print("poppet is the smallest")
+
 def main():
     jess_is_noob()
     tasty_is_good()
     specs_is_good()
+    poppet()
 
 if __name__ == '__main__':
     main()
