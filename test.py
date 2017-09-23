893875ecbcb0979045fa6c9bf61465a2aa7fadfe
tom dorrington <dorrez2k8@hotmail.co.uk>
2017-09-23 09:26:54 +0100

    Add nutkin

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ def jess_is_noob():
 def tasty_is_good():
     print("tasty is good")
 
+def nutkin():
+    print("nutkin is my favorite")
+
 def main():
     jess_is_noob()
     tasty_is_good()
+    nutkin()
 
 if __name__ == '__main__':
     main()
