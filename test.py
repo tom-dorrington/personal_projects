2932f678121bdea1cd290def1771f216e7db7564
tom dorrington <dorrez2k8@hotmail.co.uk>
2017-09-23 09:26:01 +0100

    Add specks function

--- test.py
+++ test.py
@@ -4,9 +4,13 @@ def jess_is_noob():
 def tasty_is_good():
     print("tasty is good")
 
+def specs_is_good():
+    print("specks is cool")
+
 def main():
     jess_is_noob()
     tasty_is_good()
+    specs_is_good()
 
 if __name__ == '__main__':
     main()
