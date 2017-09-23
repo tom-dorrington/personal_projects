13f80c0d2478624bcffb7992b11c3f0aadf999f3
tom dorrington <dorrez2k8@hotmail.co.uk>
2017-09-23 09:17:14 +0100

    Introduce 'tasty is good' function

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 def jess_is_noob():
     print("jess is a noob")
 
+def tasty_is_good():
+    print("tasty is good")
+
 def main():
     jess_is_noob()
+    tasty_is_good()
 
 if __name__ == '__main__':
     main()
